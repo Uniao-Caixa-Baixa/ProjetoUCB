48ccf57d9a29f901d169f01cc7432c17984f5241
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2022-06-18 14:04:19 -0300

    Cria verificação de nome no registro
    O registro verifica se o nome já está em uso

--- JS/registro.js
+++ JS/registro.js
@@ -10,8 +10,10 @@ form.addEventListener('submit', function(e){
     let senha = form.elements.senha.value;
     let senha2 = form.elements.senha2.value;
 
-    if (senha != senha2){
-        e.preventDefault();
+    if (!verificaNome(nome)){
+        form.elements.user.value = '';
+        window.alert('Nome de usuário já está em uso! Tente outro nome')
+    }else if (!verificaSenha(senha, senha2)){
         form.elements.senha.value = '';
         form.elements.senha2.value = '';
         window.alert('As senhas não combinam! Digite novamente...')
@@ -32,3 +34,20 @@ form.addEventListener('submit', function(e){
     }
 })
 
+function verificaSenha(senha1, senha2){
+    if (senha1 == senha2) {
+        return true
+    }else{
+        return false
+    }
+}
+
+function verificaNome(nome){
+    let valido = true
+    arrayUser.forEach(user => {
+        if (user['nome'] == nome){
+            valido = false
+        }
+    });
+    return valido
+}
