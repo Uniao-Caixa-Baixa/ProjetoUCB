76164e2d01e4423ea8b085ce859da124bd5a99db
Carlos Magalhães <carlosedsmagalhaes@gmail.com>
2022-06-21 10:28:52 -0300

    Criação de Update
    - Cria a tela para o usuário alterar a senha;
    - Cria o JS para validar a alteração de senha do usuário.

---
+++ JS/alterarSenha.js
@@ -0,0 +1,55 @@
+
+const form = document.querySelector('#alterarSenha');
+
+let arrayUser = JSON.parse(sessionStorage.arrayUser);
+
+let alteraSenha = 0;
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const email = form.elements.email.value;
+    let senha = form.elements.senha.value;
+    let senha2 = form.elements.senha2.value;
+
+    if (verificaEmail(email)){
+        form.elements.email.value = '';
+        window.alert('Não encontramos nenhuma conta com esse email, primeiramente faça seu registro!')
+    }else if (!verificaSenha(senha, senha2)){
+        form.elements.senha.value = '';
+        form.elements.senha2.value = '';
+        window.alert('As senhas não combinam! Digite novamente...')
+    }else{
+        while (alteraSenha<arrayUser.length){
+            console.log('alterou!');
+            if (email == arrayUser[alteraSenha]['email']){
+                console.log('alterou!');
+                arrayUser[alteraSenha]['senha'] = senha;
+            }
+            alteraSenha++;
+        }
+        
+        sessionStorage.arrayUser = JSON.stringify(arrayUser);
+        form.elements.senha.value = '';
+        form.elements.senha2.value = '';
+        window.alert('Senha alterada com sucesso!')
+    
+    }
+})
+
+function verificaSenha(senha1, senha2){
+    if (senha1 == senha2) {
+        return true
+    }else{
+        return false
+    }
+}
+
+function verificaEmail(email){
+    let valido = true
+    arrayUser.forEach(user => {
+        if (user['email'] == email){
+            valido = false
+        }
+    });
+    return valido
+}
