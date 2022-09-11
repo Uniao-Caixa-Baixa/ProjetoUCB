ee763fa9fddd5603361b6df4cf0a8328f24f5e17
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2022-09-11 02:05:40 -0300

    Organiza o dashboard em abas

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -3,17 +3,37 @@ import { UserManager } from "./users.js"
 var currentUser = UserManager.currentUser
 const sairBtn = document.querySelector('#sairBtn')
 const dashboard = document.querySelector('#dashboard')
-const admDivs = document.querySelectorAll(".adm")
+const adms = document.querySelectorAll(".adm")
 const redirect = document.querySelector('#redirect-login')
+
+const tabs = document.querySelectorAll('.tab-content')
+const tab_btns = document.querySelectorAll('.tab-btn')
+
+const showTab = (tab_index)=>{
+    tabs.forEach((tab)=>{
+        tab.style.display = 'none'
+    })
+
+    tabs[tab_index].style.display = 'flex'
+}
+
 
 if (!UserManager.isLogged) {
     redirect.style.display = 'flex'
 }else{
     dashboard.style.display = 'flex'
     if(currentUser['usuario'] == 'ADMINISTRADOR'){
-        admDivs.forEach(el => el.style.display = 'flex')
+        adms.forEach(el => el.style.display = 'flex')
+    }
+    showTab(0)
+
 }
-}
+
+tab_btns.forEach((btn, index)=>{
+    btn.addEventListener('click', ()=>{
+        showTab(index)
+    })
+})
 
 sairBtn.addEventListener('click', () => {
     UserManager.deslogar()
