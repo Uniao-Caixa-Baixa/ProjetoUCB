35fa8e0a774365da8a576cc624408c74c1263898
Bruno Vieira <brunosilverio01@gmail.com>
2022-09-29 09:21:38 -0300

    Cria pasta public para JS, CSS e imagens

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const path = require('path');
+const express = require('express');
+const { resolve } = require('path');
 
 const port = 3000
 const app = express()
 
-app.set('views', path.resolve('./views'))
+app.set('views', resolve('./views'))
 app.set('view engine', 'ejs')
+
+app.use(express.static(resolve('./public')))
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}...`)
